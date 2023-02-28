e15e9a362aaa3083459da8320d13887e8d7e6818
jessedesimone <52834672+jessedesimone@users.noreply.github.com>
2023-02-28 12:08:39 -0500

    230228

--- code/3dROIstats.sh
+++ code/3dROIstats.sh
@@ -34,3 +34,32 @@ do
         3dROIstats -mask_f2short -nzvoxels -nzmean -nzsigma -mask $roi ${infile}[${vol}] >> $out_dir/roistats_${roi}.xls
     done
 done
+
+
+# another option
+# configure directories
+top=`pwd`
+data_dir=$top/data
+mask_dir=$top/output/output_230222
+out_dir=$top/stats/roistats ; mkdir -p $out_dir
+
+# define mask files
+ROI=`cat $mask_dir/roi_list`
+
+# define subject list
+SUB=`cat $data_dir/id_subj`
+
+# compute ROIstats for each subject and each roi mask
+for roi in ${ROI[@]}
+do
+    echo "++ computing roistats for ROI $roi"
+    mask=$mask_dir/$roi/grp_wb_z_${roi}_fwer_mask.nii.gz
+
+    for sub in ${SUB[@]}
+    do
+        echo "++ computing roistats for SUBJ $sub"
+        infile=$data_dir/$sub/NETCORR_000_INDIV/WB_Z_ROI_${roi}.nii.gz[PearCorr#0]
+        3dROIstats -mask_f2short -nzvoxels -nzmean -mask $mask $infile >> $out_dir/roistats_${roi}.txt
+
+    done
+done
